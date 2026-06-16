9f48c929a0d752115d99e2aafd7f6fbd80c116d9
Shao Huang <sthuang622@gmail.com>
2026-06-16 15:32:30 +0800

    wip jwt service setup

--- Services/Authorization/Implementations/JwtService.cs
+++ Services/Authorization/Implementations/JwtService.cs
@@ -1,4 +1,44 @@
-﻿namespace Online_Store_Backend_WebAPI.Services.Authorization.Implementations {
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Online_Store_Backend_WebAPI.Services.Authorization.Implementations {
     public class JwtService {
+
+        private readonly string _secretKey = "your-super-secret-key-that-is-at-least-32-chars";
+        private readonly string _issuer = "your-app";
+        private readonly string _audience = "your-app-users";
+
+        public string CreateToken(string userId, string email) {
+            var role = ""; //get from db
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId),
+                new Claim(JwtRegisteredClaimNames.Email, email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Role, role)
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_secretKey)
+            );
+
+            var credentials = new SigningCredentials(
+                key,
+                SecurityAlgorithms.HmacSha256
+            );
+
+            var token = new JwtSecurityToken(
+                issuer: _issuer,
+                audience: _audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
